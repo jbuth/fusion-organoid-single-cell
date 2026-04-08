a51198eb5710bd60d8975b4ab7541560326860b7
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-07 18:52:22 -0700

    Create 01_cellranger_count_batch_run.sh

---
+++ 01_cellranger_count_batch_run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+## Save this script as:
+## /path/to/fusion-organoid-single-cell/code/01_cellranger_count_batch_run.sh
+
+## To execute, cd into the code folder, then type:
+## ./01_cellranger_count_batch_run.sh
+
+## Description:
+## Loops through sample folders in /fastq and submits one qsub job per sample
+## qsub runs cellranger count via the script Step1_qsub_cellranger_count.sh
+
+## ------ Setup directories ------- ##
+
+## Base directory: /path/to/
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+
+## Subdirectories
+CODE_DIR=${BASE_DIR}/fusion-organoid-single-cell/code
+DATA_DIR=${BASE_DIR}/fusion-organoid-single-cell/fastq
+
+## --- Data directory structure --- ##
+
+## Example:
+## ${DATA_DIR}/D100_Ctrl_docked_1
+##      /__D100_Ctrl_docked_1.mro
+##      /D100_Ctrl_docked_1_S5_L001_I1_001.fastq.gz
+##      /D100_Ctrl_docked_1_S5_L001_R1_001.fastq.gz
+##      /D100_Ctrl_docked_1_S5_L001_R2_001.fastq.gz
+##      /D100_Ctrl_docked_1_S5_L002_I1_001.fastq.gz
+##      /D100_Ctrl_docked_1_S5_L002_R1_001.fastq.gz
+##      /D100_Ctrl_docked_1_S5_L002_R2_001.fastq.gz
+
+## --- Change to data directory --- ##
+
+cd "${DATA_DIR} || exit" || exit
+
+## ---- Batch submit qsub jobs ---- ##
+
+for file in */; do
+  
+  name=${file%/}  ## save the sample folder name
+  echo "${name}"  ## print the name
+  
+  ## directory for log stdout (-o) and stderr (-e)
+  ## runtime, memory per core, highp runs on lab purchased compute nodes
+  ## shared memory 16G x 8 cores = 128G total
+  qsub -o "${CODE_DIR}"/log -e "${CODE_DIR}"/log \
+  -l h_rt=8:00:00,h_data=16G,highp \
+  -pe shared 8 "${CODE_DIR}"/Step1_qsub_cellranger_count.sh "${name}"
+  
+done
