58a4ceccb146f44d208f37ad9f640390607e2b8c
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-07 22:23:08 -0700

    Update 01_cellranger_count.sh

--- 01_cellranger_count.sh
+++ 01_cellranger_count.sh
@@ -2,15 +2,18 @@
 
 name="$1" # sample folder name
 BASE_DIR="$2" # base directory
-DATA_DIR="${BASE_DIR}/fusion-organoid-single-cell/fastq"
-mkdir -p "${BASE_DIR}/fusion-organoid-single-cell/cellranger"
-OUT_DIR="${BASE_DIR}/fusion-organoid-single-cell/cellranger"
+PROJECT_DIR="${BASE_DIR}/fusion-organoid-single-cell"
 
-REF_DATA="${BASE_DIR}/fusion-organoid-single-cell/annotation/refdata-gex-GRCh38-2020-A"
-CELLRANGER_DIR="${BASE_DIR}/fusion-organoid-single-cell/bin/cellranger-4.0.0"
+CELLRANGER_DIR="${PROJECT_DIR}/bin/cellranger-4.0.0"
+REF_DATA="${PROJECT_DIR}/annotation/refdata-gex-GRCh38-2020-A"
+DATA_DIR="${PROJECT_DIR}/fastq"
 FASTQ_PATH="${DATA_DIR}/${name}"
 
+mkdir -p "${PROJECT_DIR}/cellranger"
+OUT_DIR="${PROJECT_DIR}/cellranger"
 cd "${OUT_DIR}" || exit
+
+echo "Running cellranger count for sample: ${name}"
 
 "${CELLRANGER_DIR}/cellranger" count \
   --id="${name}" \
