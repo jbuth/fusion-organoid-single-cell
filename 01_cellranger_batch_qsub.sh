6d4aece22038f1a2be998a62a104d548451d115d
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-07 21:21:50 -0700

    Update 01_cellranger_batch_qsub.sh

--- 01_cellranger_batch_qsub.sh
+++ 01_cellranger_batch_qsub.sh
@@ -40,14 +40,16 @@ cd "${DATA_DIR}" || exit
 for file in */; do
   
   name="${file%/}"  ## save the sample folder name
-  echo "${name}"  ## print the name
+  echo "${name}"
   
   ## directory for log stdout (-o) and stderr (-e)
   ## runtime, memory per core, highp runs on lab purchased compute nodes
-  ## shared memory 16G x 8 cores = 128G total
-  qsub -o "${CODE_DIR}"/log \
-  -e "${CODE_DIR}"/log \
+  ## shared memory request for 8 cores with 16G per core (~128G total)
+  qsub \
+  -o "${CODE_DIR}/log" \
+  -e "${CODE_DIR}/log" \
   -l h_rt=8:00:00,h_data=16G,highp \
-  -pe shared 8 "${CODE_DIR}"/01_cellranger_count.sh "${name}" "${BASE_DIR}"
+  -pe shared 8 \
+  "${CODE_DIR}/01_cellranger_count.sh" "${name}" "${BASE_DIR}"
   
 done
