623dbdb883896c865cea4ca606916b296921c3a0
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-07 22:03:51 -0700

    Update 01_cellranger_count.sh

--- 01_cellranger_count.sh
+++ 01_cellranger_count.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-name="$1" # sample folder name from previous script
-BASE_DIR="$2" # saved base directory from previous script
+name="$1" # sample folder name
+BASE_DIR="$2" # base directory
 DATA_DIR="${BASE_DIR}/fusion-organoid-single-cell/fastq"
 
 REF_DATA="${BASE_DIR}/fusion-organoid-single-cell/annotation/refdata-gex-GRCh38-2020-A"
 CELLRANGER_DIR="${BASE_DIR}/fusion-organoid-single-cell/bin/cellranger-4.0.0"
 FASTQ_PATH="${DATA_DIR}/${name}"
 
-cd "${DATA_DIR}/${name}" || exit
-
-"${CELLRANGER_DIR}"/cellranger count --id="${name}" --transcriptome="${REF_DATA}" --fastqs="${FASTQ_PATH}"
+"${CELLRANGER_DIR}/cellranger" count \
+  --id="${name}" \
+  --transcriptome="${REF_DATA}" \
+  --fastqs="${FASTQ_PATH}" \
+  --localcores=8 \
+  --localmem=120
